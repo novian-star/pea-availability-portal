40686ee94809ea067b812d206db0762e6d7a60e8
Novian <95335310+novian-star@users.noreply.github.com>
2025-09-01 14:57:46 +0700

    🔨 refactor: refactor equipment statistics fetching logic for improved readability and maintainability

--- server/api/statistics/index.get.ts
+++ server/api/statistics/index.get.ts
@@ -1,62 +1,62 @@
 export default defineEventHandler({
-  onRequest: [requireUserSessionEventHandler()],
+	onRequest: [requireUserSessionEventHandler()],
 
-  handler: async (_event) => {
-    const { statisticsSheetId, equipmentsSheetId } = useRuntimeConfig();
-    // const userSession = await requireUserSession(event);
+	handler: async (_event) => {
+		const { statisticsSheetId, equipmentsSheetId } = useRuntimeConfig();
+		// const userSession = await requireUserSession(event);
 
-    try {
-      const auth = useGoogleAuth([
-        'https://www.googleapis.com/auth/spreadsheets.readonly',
-      ]);
-      const sheets = useGoogleSheets(auth);
+		try {
+			const auth = useGoogleAuth([
+				'https://www.googleapis.com/auth/spreadsheets.readonly',
+			]);
+			const sheets = useGoogleSheets(auth);
 
-      const result = await Promise.all([
-        sheets.spreadsheets.values.get({
-          spreadsheetId: statisticsSheetId,
-          range: "' PEA AVA Ranking FRTU'!A1:E",
-        }),
-        // Timestamp
-        sheets.spreadsheets.values.get({
-          spreadsheetId: equipmentsSheetId,
-          range: "'ข้อมูล ณ เวลา'!A1:A2",
-        }),
-      ]);
-      result[0].data.values?.splice(0, 1); // Remove header row
+			const result = await Promise.all([
+				sheets.spreadsheets.values.get({
+					spreadsheetId: statisticsSheetId,
+					range: "' PEA AVA Ranking FRTU'!A1:E",
+				}),
+				// Timestamp
+				sheets.spreadsheets.values.get({
+					spreadsheetId: equipmentsSheetId,
+					range: "'ข้อมูล ณ เวลา'!A1:A2",
+				}),
+			]);
+			result[0].data.values?.splice(0, 1); // Remove header row
 
-      const rawTimestamp = String(result[1].data.values?.[1]?.[0]);
-      const rawDate = rawTimestamp.slice(0, 9);
-      const [year, month, day] = rawDate.split(/\//g).toReversed();
-      const date = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
-      const time = rawTimestamp.slice(-8, -3);
-      const timestamp = new Date(`${date}T${time}:00+07:00`);
+			const rawTimestamp = String(result[1].data.values?.[1]?.[0]);
+			const rawDate = rawTimestamp.slice(0, 10).replace(/[^/0-9]/g, '');
+			const [year, month, day] = rawDate.split(/\//g).toReversed();
+			const date = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+			const time = rawTimestamp.slice(-8, -3);
+			const timestamp = new Date(`${date}T${time}:00+07:00`);
 
-      const data = result[0].data.values?.map((row) => {
-        return {
-          region: String(row[0]),
-          online: Number(row[1]),
-          total: Number(row[2]),
-          percentage: Number(row[3]),
-          ranking: Number(row[4]),
-        };
-      });
-      if (!data) {
-        throw createError({
-          statusCode: 404,
-          statusMessage: 'No data found',
-        });
-      }
+			const data = result[0].data.values?.map((row) => {
+				return {
+					region: String(row[0]),
+					online: Number(row[1]),
+					total: Number(row[2]),
+					percentage: Number(row[3]),
+					ranking: Number(row[4]),
+				};
+			});
+			if (!data) {
+				throw createError({
+					statusCode: 404,
+					statusMessage: 'No data found',
+				});
+			}
 
-      return {
-        data: data,
-        timestamp: timestamp,
-      };
-    } catch (error) {
-      console.error('Error fetching availability statistics:', error);
-      throw createError({
-        statusCode: 500,
-        statusMessage: 'Internal Server Error',
-      });
-    }
-  },
+			return {
+				data: data,
+				timestamp: timestamp,
+			};
+		} catch (error) {
+			console.error('Error fetching availability statistics:', error);
+			throw createError({
+				statusCode: 500,
+				statusMessage: 'Internal Server Error',
+			});
+		}
+	},
 });
