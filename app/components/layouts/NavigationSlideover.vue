1820b43a6f924c1b5c1e8de665d8815947a602da
Novian <95335310+novian-star@users.noreply.github.com>
2025-09-25 08:32:38 +0700

    ✨ feat: enhance layout structure and styling for NavigationSlideover component

--- app/components/layouts/NavigationSlideover.vue
+++ app/components/layouts/NavigationSlideover.vue
@@ -1,31 +1,37 @@
 <script setup lang="ts">
 const emit = defineEmits<{
-  close: [];
+	close: [];
 }>();
 
 const { items } = useNavigation();
 
 const route = useRoute();
 watch(
-  () => route.path,
-  () => emit('close')
+	() => route.path,
+	() => emit('close')
 );
 </script>
 
 <template>
-  <USlideover side="left" :ui="{ content: 'w-80 border-e' }">
-    <template #content>
-      <header class="flex items-center justify-between h-16 px-4">
-        <UButton icon="lucide:menu" variant="ghost" />
-        <UButton icon="lucide:x" variant="ghost" @click="emit('close')" />
-      </header>
-      <div class="p-4">
-        <UNavigationMenu
-          :items="items"
-          orientation="vertical"
-          :ui="{ root: 'gap-4', list: 'space-y-2' }"
-        />
-      </div>
-    </template>
-  </USlideover>
+	<USlideover side="left" :ui="{ content: 'flex flex-col w-80 border-e' }">
+		<template #content>
+			<header class="flex items-center justify-between h-16 px-4">
+				<UButton icon="lucide:menu" variant="ghost" />
+				<UButton icon="lucide:x" variant="ghost" @click="emit('close')" />
+			</header>
+			<div class="flex-1 p-4">
+				<UNavigationMenu
+					:items="items"
+					orientation="vertical"
+					:ui="{ root: 'gap-4', list: 'space-y-2' }"
+				/>
+			</div>
+			<footer
+				class="flex items-center justify-between h-16 px-4 text-sm text-muted"
+			>
+				<p>พัฒนาโดย ผจศ.กปบ.(น3)</p>
+				<p>Version 5.0.1</p>
+			</footer>
+		</template>
+	</USlideover>
 </template>
