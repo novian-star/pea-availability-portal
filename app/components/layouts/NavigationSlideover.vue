267236cad21ead6c755d9c25ed19d3bf872d43a9
novian <humanityisyour@gmail.com>
2026-03-24 17:27:22 +0700

    🧹 chore: update footer version to 5.2.0

--- app/components/layouts/NavigationSlideover.vue
+++ app/components/layouts/NavigationSlideover.vue
@@ -1,37 +1,37 @@
 <script setup lang="ts">
 const emit = defineEmits<{
-	close: [];
+  close: [];
 }>();
 
 const { items } = useNavigation();
 
 const route = useRoute();
 watch(
-	() => route.path,
+  () => route.path,
 	() => emit('close')
 );
 </script>
 
 <template>
-	<USlideover side="left" :ui="{ content: 'flex flex-col w-80 border-e' }">
-		<template #content>
-			<header class="flex items-center justify-between h-16 px-4">
-				<UButton icon="lucide:menu" variant="ghost" />
-				<UButton icon="lucide:x" variant="ghost" @click="emit('close')" />
-			</header>
-			<div class="flex-1 p-4">
-				<UNavigationMenu
-					:items="items"
-					orientation="vertical"
-					:ui="{ root: 'gap-4', list: 'space-y-2' }"
-				/>
-			</div>
-			<footer
-				class="flex items-center justify-between h-16 px-4 text-sm text-muted"
-			>
-				<p>พัฒนาโดย ผจศ.กปบ.(น3)</p>
-				<p>Version 5.1.0</p>
-			</footer>
-		</template>
-	</USlideover>
+  <USlideover side="left" :ui="{ content: 'flex flex-col w-80 border-e' }">
+    <template #content>
+      <header class="flex items-center justify-between h-16 px-4">
+        <UButton icon="lucide:menu" variant="ghost" />
+        <UButton icon="lucide:x" variant="ghost" @click="emit('close')" />
+      </header>
+      <div class="flex-1 p-4">
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
+          :ui="{ root: 'gap-4', list: 'space-y-2' }"
+        />
+      </div>
+      <footer
+        class="flex items-center justify-between h-16 px-4 text-sm text-muted"
+      >
+        <p>พัฒนาโดย ผจศ.กปบ.(น3)</p>
+        <p>Version 5.2.0</p>
+      </footer>
+    </template>
+  </USlideover>
 </template>
