b3115954181870e2004ee62b1c3371e6ba6bfd3e
novian <humanityisyour@gmail.com>
2026-03-26 15:05:45 +0700

    ✨ feat: create notice table and update related migration files

--- .drizzle/migrations/0003_workable_slipstream.sql
+++ .drizzle/migrations/0003_workable_slipstream.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "notice" (
 	"id" varchar(255) PRIMARY KEY NOT NULL,
-	"title" varchar(255) NOT NULL,
-	"content" text NOT NULL,
+	"title" varchar(255),
+	"content" text,
 	"is_enabled" boolean DEFAULT false NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
--- .drizzle/migrations/0004_closed_pixie.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "notice" ALTER COLUMN "title" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "notice" ALTER COLUMN "content" DROP NOT NULL;
